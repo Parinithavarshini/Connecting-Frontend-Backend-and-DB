69c968426f822741e0af9daf827ffd4de0953494
Parinitha Varshini.S <149925989+Parinithavarshini@users.noreply.github.com>
2025-01-04 19:59:01 +0530

    Media queries is created.

---
+++ App.jsx
@@ -0,0 +1,28 @@
+// import { useState } from 'react'
+// import './App.css'
+// import Login from './Login'
+
+// function App() {
+//   return (
+//     <>
+//       <Login />
+//     </>
+//   )
+// }
+
+// export default App
+
+import React from 'react';
+import Page from './Page';
+import './Page.css';
+
+const App = () => {
+  return (
+    <div>
+      <Page />
+    </div>
+  );
+};
+
+export default App;
+
---
+++ page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Page = () => {
+  return (
+    <div className="image-container">
+      <img src="https://dummyimage.com/300x200/000/fff&text=Image+1" alt="Image 1" className="responsive-image" />
+      <img src="https://dummyimage.com/300x200/000/fff&text=Image+2" alt="Image 2" className="responsive-image" />
+      <img src="https://dummyimage.com/300x200/000/fff&text=Image+3" alt="Image 3" className="responsive-image" />
+    </div>
+  );
+};
+
+export default Page;
